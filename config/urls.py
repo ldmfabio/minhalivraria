664f6fbf2b9fcd7a91c07ed76ae022da724bb3d0
ldmfabio <ldmfabio@gmail.com>
2023-09-27 15:28:35 -0300

    Ponto 8.4.2

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from minhalivraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
+
+router = DefaultRouter()
+router.register('categorias', CategoriaViewSet)
+router.register('editoras', EditoraViewSet)
+router.register('autores', AutorViewSet)
+router.register('livros', LivroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
--- minhalivraria/models.py
+++ minhalivraria/models.py
@@ -6,6 +6,10 @@ class Categoria(models.Model):
     def __str__(self):
         return self.descricao
     
+    class Meta:
+        verbose_name_plural = 'Categorias'
+        verbose_name = 'Categoria'
+    
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
@@ -13,3 +17,36 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+    
+    class Meta:
+        verbose_name_plural = 'Editoras'
+        verbose_name = 'Editora'
+    
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name_plural = 'Autores'
+        verbose_name = 'Autor'
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros')
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros')
+
+    def __str__(self) -> str:
+        return f"{self.titulo} ({self.quantidade})"
+    
+    class Meta:
+        verbose_name_plural = 'Livros'
+        verbose_name = 'Livro'
+        ordering = ['titulo']
